4898897c07cb7a3efaaf2b8827a7d493f01243ed
Jarangutan <53702919+jarangutan@users.noreply.github.com>
2020-09-26 03:28:59 -0400

    workshop done phew

--- buckets/backend.tf
+++ buckets/backend.tf
@@ -1,7 +1,9 @@
 # Uncomment the following terraform backend block to use Terraform Cloud to host the backend statefile
 # Make sure to:
+# - Rename dev.tfvars to dev.auto.tfvars and prod.tfvars to prod.auto.tfvars
 # - Create your orgnization in Terraform Cloud
-# - Create your workspace in your terminal using `terraform workspace new name` where name is your workspace name (i.e dev, prod, so on)
+# - Log on to terraform cloud in your terminal `terraform login`
+# - Create your workspace in your terminal using `terraform workspace new dev` where dev is your workspace name (i.e dev, prod, so on)
 # - `terraform init` to initialize your workspace
 # - Add the AWS Credentials for your programmatic user to your environment variables 
 #   for your new workspace in Terraform Cloud-->workspace-->your_workspace-->variables.
@@ -9,7 +11,7 @@
 #     - AWS_ACCESS_KEY_ID - sensitive
 #     - AWS_SECRET_ACCESS_KEY - sensitive
 # - Invite your fellow hackers to your organization!
-
+# - Run `terraform apply` and deploy your resources :D!
 
 # terraform {
 #   backend "remote" {
